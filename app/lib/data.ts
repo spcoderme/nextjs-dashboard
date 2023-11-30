7e810cf3c558b497285ab10f28f9a4c728785aaa
Suraj Pawar <88433599+spcoderme@users.noreply.github.com>
2023-11-30 15:57:35 +0530

    Changed something else about .env file

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,5 @@
 import { sql } from '@vercel/postgres';
+
 import {
   CustomerField,
   CustomersTableType,
@@ -34,12 +35,13 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // Fetch the last 5 invoices, sorted by date
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+SELECT invoices.amount, customers.name, customers.image_url, customers.email
+FROM invoices
+JOIN customers ON invoices.customer_id = customers.id
+ORDER BY invoices.date DESC
+LIMIT 5`;
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
